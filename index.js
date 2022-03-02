dfc05b32744cc8168389f7fe94ec5d7fe9d901cd
coryla <cory.lance.anderson@gmail.com>
2022-03-02 12:12:56 -0600

    clearBoard added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 //row and columns (think arrays 2d)
 
 //beginning of object
+
 const ticTacToe = {
+  players: ["x", "o"],
   board: [
     [null, null, null],
 
@@ -9,25 +11,48 @@ const ticTacToe = {
 
     [null, null, null],
   ],
-  /* winning conditions will come from positions definined within the array of arrays. win condition is met if any player occupies the following array coordiates: 
+};
+
+console.log("board start", ticTacToe.board);
+
+/* winning conditions will come from positions definined within the array of arrays. win condition is met if any player occupies the following array coordiates: 
 
 x:
-[0,0] [0,1] [0,2] - can i just log the whole array? same with midde and bottom row?
+[0][0] , [0] [1] , [0] [2] etc...
+
+- can i just log the whole array? same with midde and bottom row?
 
 y:
-[0,0] [1,0] [2,0] - can i group these as well vertically? or do they need to be individually acknowledged - see x note for contrast? 
+[0] [0] , [1] [0] , [2] [0] etc...
+
+- can i group these as well vertically? or do they need to be individually acknowledged - see x note for contrast? 
 
 bit X: 
-[0,0] [1,1], [2,2]
-[0,2] [1,1], [2,0]
+[0] [0] [1] [1], [2] [2]
+[0] [2] [1] [1], [2] [0]
+
 
 
 */
 
-  //move method
-  move: function (character, rowNumber, columnNumber) {},
+// table.addEventListener("click", clickTest);
 
-  //clear method - can i call the original board?
-  clear: function (board) {},
-};
-//end of object
+/*focus on event listeners for putting x or o in box - refer to pixelate and whack a mole. */
+
+//move method and event listener
+
+function clickTest(Event) {
+  console.log("clicked");
+}
+
+// playerMove: function (player, rowNumber, columnNumber) {},
+
+//clear method - can i call the original board?
+
+function clearBoard() {
+  ticTacToe.board = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ];
+}
