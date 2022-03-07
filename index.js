6e059f6408b4bbec04614211c0473ddff478c5e2
coryla <cory.lance.anderson@gmail.com>
2022-03-06 21:24:33 -0600

    project delivery

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const table = document.getElementById("table");
+const restartButton = document.getElementById("restartButton");
 
-const winningConditions = console.log("table", table);
+
+
+//state
 
 const ticTacToe = {
   players: ["x", "o"],
@@ -12,29 +15,55 @@ const ticTacToe = {
     [null, null, null],
   ],
   currentPlayer: "x",
+
+  counter: 0,
 };
+//create player
+const player = (name) => {
+  name = name;
+  return { name };
+};
+let playerX = player("");
+let playerY = player("");
 
-console.log("board start", ticTacToe.board);
+//fix this button!!
+restartButton.addEventListener("click", clearBoard);
 
-/* winning conditions will come from positions definined within the array of arrays. win condition is met if any player occupies the following array coordiates: 
+//I had this logging working for x but broke it somewhere...
+function checkRowX(x = ticTacToe.board) {
+  for (let i = 0; i < x.length; i++) { 
+    if (x[i].join("") = "xxx") {
+      alert("X Beats O");
+    }
+  }
+}
 
-x:
-[0][0] , [0] [1] , [0] [2] etc...
+// look into 'transpose' function to use for checkColumn 
 
-- can i just log the whole array? same with midde and bottom row?
+function transpose() {
+  let arr = [];
+  for (let i = 0; i < ticTacToe.board.length; i++) {
+    arr.push([]);
+    for (let j = 0; j < ticTacToe.board.length; j++) {
+      arr[i].push(ticTacToe.board[j][i]);
+    }
+  }
+  return arr;
+}
 
-y:
-[0] [0] , [1] [0] , [2] [0] etc...
+function checkForWinner() {
+  checkColumn();
+  checkRow();
+  //checkDiagnal()
+}
 
-- can i group these as well vertically? or do they need to be individually acknowledged - see x note for contrast? 
+function checkColumn() {
+  let newArray = transpose();
+  console.log(newArray);
+  checkRow(newArray);
+}
 
-bit X: 
-[0] [0] [1] [1], [2] [2]
-[0] [2] [1] [1], [2] [0]
-
-
-
-*/
+function checkDiagnal() {}
 
 //add button
 function clearBoard() {
@@ -42,16 +71,45 @@ function clearBoard() {
     [null, null, null],
     [null, null, null],
     [null, null, null],
-  ];
+  ] 
+  
 }
 
-table.addEventListener("click", function (event) {
-  console.log(event.target);
-  event.target.innerText = ticTacToe.currentPlayer;
-
-  if (ticTacToe.currentPlayer === "x") {
-    ticTacToe.currentPlayer = "o";
-  } else {
-    ticTacToe.currentPlayer = "x";
+function renderBoard() {
+  for (let i = 0; i < ticTacToe.board.length; i++) {
+    // console.log("Im in the outer for loop and I is:", i);
+    for (let j = 0; j < ticTacToe.board.length; j++) {
+      // console.log("Im in the inner for loop and j is:", j);
+      const currCell = document.getElementById(`${i}${j}`);
+      currCell.innerText = ticTacToe.board[i][j];
+    }
   }
-});
+}
+
+//if (line 50) there is not inner text move foward in function(line 51).
+table.addEventListener("click", function (event) {
+  if (!event.target.innerText) {
+    const row = event.target.id[0];
+    const col = event.target.id[1];
+    ticTacToe.board[row][col] = ticTacToe.currentPlayer;
+    renderBoard();
+    // console.log(ticTacToe.board);
+    if (ticTacToe.currentPlayer === "x") {
+      ticTacToe.currentPlayer = "o";
+      ticTacToe.counter++;
+    } else {
+      ticTacToe.currentPlayer = "x";
+    }
+    // console.log(ticTacToe.counter);
+    if (ticTacToe.counter >= 3) {
+      checkForWinner();
+    }
+  }
+
+  // checkRow(ticTacToe.board[0], "x");
+  // checkRow(ticTacToe.board[0], "o");
+  // checkRow(ticTacToe.board[1], "x");
+  // checkRow(ticTacToe.board[1], "o");
+  // checkRow(ticTacToe.board[2], "x");
+  // checkRow(ticTacToe.board[2], "o");
+})
